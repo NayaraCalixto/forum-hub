c3b5b47d65d6f3bc5445f11e4c24c8c9aaf890b5
Nayara Calixto <nayaranec2@gmail.com>
2025-08-12 21:34:28 -0300

    Iniciando autenticação.

--- src/main/java/br/com/alura/forumhub/dto/DadosAtualizarTopico.java
+++ src/main/java/br/com/alura/forumhub/dto/DadosAtualizarTopico.java
@@ -1,21 +1,15 @@
 package br.com.alura.forumhub.dto;
 
 import br.com.alura.forumhub.model.topico.Status;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record DadosAtualizarTopico(
     @NotNull 
     Long id,
-    @NotBlank 
     String titulo,
-    @NotBlank 
     String mensagem,
-    @NotNull
     Status status,
-    @NotNull 
     Long autorId,
-    @NotNull 
     Long cursoId) {
 
 }
--- src/main/java/br/com/alura/forumhub/repository/usuario/UsuarioRepository.java
+++ src/main/java/br/com/alura/forumhub/repository/usuario/UsuarioRepository.java
@@ -1,9 +1,12 @@
 package br.com.alura.forumhub.repository.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import br.com.alura.forumhub.model.usuario.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+    UserDetails findByLogin(String login);
 
 }
